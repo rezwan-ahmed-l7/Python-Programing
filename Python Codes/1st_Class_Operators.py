13fb28ab38d96c673a3a426d8b8f3e2170adb79a
Rezwan <rezwanahmedtorabee@gmail.com>
2026-07-19 23:08:24 +0600

    Python

---
+++ Python Codes/1st_Class_Operators.py
@@ -0,0 +1,38 @@
+# Arithmetic Operators
+
+a = 5
+b = 2
+
+sum = (a + b)
+
+print ("Sum of a and b is: ", sum)
+print ("Sum: ", a + b)   # Sum
+
+print ("Modulus: ", a % b)   # Modulus to get remainder
+print ("Exponentiation: ", a ** b)  # Exponentiation to get power
+print ("Floor Division: ", a // b)  # Floor division to get integer value
+print ( )
+
+# Assignment Operators
+
+c = 10
+d = 5
+
+c **= d  # c = c ** d
+print ("c to the power of d: ", c)
+
+# Logical Operators
+
+e = 10
+f = 5
+
+print (not (e > f))
+print (e > f and e < 15)
+print (e > f or e < 15)
+
+value1 = True
+value2 = False
+
+print (value1 and value2)
+print (value1 or value2)
+print (not value1)
---
+++ Python Codes/1st_Class_Veriables_Datatypes.py
@@ -0,0 +1,22 @@
+""" Variables and Datatypes """
+
+name = "Paris"
+age = 25
+gpa = 3.50
+Human = True
+pets = None
+
+print ("My name is: ", name)
+print ("My age is: ", age)
+print ("My GPA is: ", gpa)
+print ("Am I a human? ", Human)
+print ("Do I have pets? ", pets)
+
+print ( )
+# Data Type of variables
+
+print (type(name))
+print (type(age))
+print (type(gpa))
+print (type(Human))
+print (type(pets))
