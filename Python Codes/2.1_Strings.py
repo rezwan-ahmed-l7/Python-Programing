ad33af6302368d962b0febce09a030cacc5e9343
Rezwan <rezwanahmedtorabee@gmail.com>
2026-07-24 18:03:03 +0600

    Python

---
+++ Python Codes/2.1_Strings.py
@@ -0,0 +1,22 @@
+# String syntax in python
+
+str1 = "Style 1"
+str2 = 'Style 2'
+str3 = '''Style 3'''
+
+# Reasons for different string syntax in python
+
+print (" I'm going home ")
+
+print (' He said "I will go home" ')
+
+print (''' He said "let's go home" ''')
+
+print()
+# String 
+
+a = " I'm Paris. \n Nice to meet you. "
+b = " I'm Paris,\t nice to meet you. "
+
+print (a)
+print (b)
